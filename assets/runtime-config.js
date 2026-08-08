0abf0f65db3567084679a2cbab43ec4ba528253b
Nduah89 <nduah898@gmail.com>
2026-08-08 13:34:37 +0000

    Delete assets directory

--- assets/runtime-config.js
+++
@@ -1,1 +0,0 @@
-window.NIS_CONFIG={supabaseUrl:"https://hcqjcozjjwkowrahcpjj.supabase.co",supabaseKey:"sb_publishable_Xq04ELNHNfYbBZsiZdwNzw_vx5hmWj8",canonicalUrl:"https://nduah89.github.io/nis-child-development-centre/",version:"1.2.0"};
